7228d76796a8f3d3228cfbe661fc8bdb9a4ff1b6
Richard Chong <142849499+RichardCh971@users.noreply.github.com>
2025-04-07 15:47:10 -0400

    Added a Navbar
    Created a Navbar and it has mobile mode when you make the window smaller and when it is bigger it is in web mode. To test it make your window smaller. Also, created and added a logo as well.

---
+++ app.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const menuLinks2 = document.querySelector('.navbar')
+
+const nav = document.querySelector('.navbar')
+let lastScrollY = window.scrollY;
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    menuLinks2.classList.toggle('active');
+});
+
+window.addEventListener("scroll", () => {
+    if(lastScrollY < window.scrollY){
+        nav.classList.add("navHidden");
+    } else {
+        nav.classList.remove("navHidden")
+    }
+    lastScrollY = window.scrollY;
+});
